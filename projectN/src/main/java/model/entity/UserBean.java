2573c8c3adb1179dd8f50232e438090b769addc3
PC_User <PC_User@192.168.10.107>
2026-05-28 22:43:42 +0900

    Bean,DAO修正

--- projectN/src/main/java/model/entity/UserBean.java
+++ projectN/src/main/java/model/entity/UserBean.java
@@ -2,7 +2,7 @@ package model.entity;
 
 public class UserBean {
 	private String user_id;
-	private String post_id;
+	private int post_id;
 	
 	public String getUser_id() {
 		return user_id;
@@ -10,10 +10,10 @@ public class UserBean {
 	public void setUser_id(String user_id) {
 		this.user_id = user_id;
 	}
-	public String getPost_id() {
+	public int getPost_id() {
 		return post_id;
 	}
-	public void setPost_id(String post_id) {
+	public void setPost_id(int post_id) {
 		this.post_id = post_id;
 	}	
 }
