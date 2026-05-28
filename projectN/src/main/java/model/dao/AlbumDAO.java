a568819969487aab0bbda0aede1b16f4dd010d4a
emtech-user <emtech-user@PC-number>
2026-05-28 15:58:54 +0900

    初回コミット：たけ

--- projectN/src/main/java/model/dao/AlbumDAO.java
+++ projectN/src/main/java/model/dao/AlbumDAO.java
@@ -1,5 +1,28 @@
 package model.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class AlbumDAO {
+	public int updateAlbum(String user_id, int Album_id)throws SQLException, ClassNotFoundException{
+		int processingNumber = 0;
+		
+		String sql = "UPDATE t_album SET album_name = ?,trip_start =?, trip_end = ?,companion = ?, memo = ?";
+		
+		try(Connection con = ConnectionManager.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql)){
+			
+			pstmt.setString(1,album_name);
+			pstmt.setDate(2,trip_start);
+			pstmt.setDate(3,trip_end);
+			pstmt.setString(4,companion);
+			pstmt.setString(5,memo);
+			
+			processingNumber = pstmt.executeUpdate();
+			
+		}
+		return processingNumber;
+	}
 
 }
