fb31092f4b9369e5869e1ff21c3f36dcaaf430b7
emtech-user <emtech-user@PC-number>
2026-05-28 15:15:48 +0900

    insertPhoto追加:吉沢

--- projectN/src/main/java/model/dao/PhotoDAO.java
+++ projectN/src/main/java/model/dao/PhotoDAO.java
@@ -1,5 +1,30 @@
 package model.dao;
 
-public class PhotoDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+public class PhotoDAO {
+	//写真を追加するメソッド
+	public int insertPhoto(int album_id, int area_id, String photo_data)throws ClassNotFoundException, SQLException {
+		
+		String sql = "INSERT INTO t_photo VALUES (null, ?, ?. ?, 0, null)";
+		
+		int count = 0;
+		
+		try(Connection con = ConnectionManager.getConnection();
+				PreparedStatement pstmt = con.prepareStatement("INSERT INTO m_employee VALUES(?, ?, ?, ?)");){
+			
+
+						// プレースホルダへの値の設定
+						pstmt.setInt(1, album_id);
+						pstmt.setInt(2, area_id);
+						pstmt.setString(3, photo_data);
+	
+						// SQLステートメントの実行
+						count = pstmt.executeUpdate();
+		}
+		
+		return count;
+	}
 }
