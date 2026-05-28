fc61b1fb749442d032ab41892e1e651a7d159e63
emtech-user <emtech-user@PC-number>
2026-05-28 16:29:54 +0900

    bean完成

--- projectN/src/main/java/model/entity/AlbumBean.java
+++ projectN/src/main/java/model/entity/AlbumBean.java
@@ -1,5 +1,91 @@
 package model.entity;
 
+import java.time.LocalDate;
+
 public class AlbumBean {
+	private String user_id;
+	private int album_id;
+	private int area_id;
+	private String area_name;
+	private LocalDate trip_start;
+	private LocalDate trip_end;
+	private String companion;
+	private String album_name;
+	private String memo;
+	
+	public AlbumBean() {
+}
+
+	public String getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+
+	public int getAlbum_id() {
+		return album_id;
+	}
+
+	public void setAlbum_id(int album_id) {
+		this.album_id = album_id;
+	}
+
+	public int getArea_id() {
+		return area_id;
+	}
+
+	public String getArea_name() {
+		return area_name;
+	}
+
+	public void setArea_name(String area_name) {
+		this.area_name = area_name;
+	}
+
+	public LocalDate getTrip_start() {
+		return trip_start;
+	}
+
+	public void setTrip_start(LocalDate trip_start) {
+		this.trip_start = trip_start;
+	}
+
+	public LocalDate getTrip_end() {
+		return trip_end;
+	}
+
+	public void setTrip_end(LocalDate trip_end) {
+		this.trip_end = trip_end;
+	}
+
+	public String getCompanion() {
+		return companion;
+	}
+
+	public void setCompanion(String companion) {
+		this.companion = companion;
+	}
+
+	public String getAlbum_name() {
+		return album_name;
+	}
+
+	public void setAlbum_name(String album_name) {
+		this.album_name = album_name;
+	}
+
+	public String getMemo() {
+		return memo;
+	}
+
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+
+	public void setArea_id(int area_id) {
+		this.area_id = area_id;
+	}
 
 }
--- projectN/src/main/java/model/entity/PhotoBean.java
+++ projectN/src/main/java/model/entity/PhotoBean.java
@@ -1,5 +1,71 @@
 package model.entity;
 
 public class PhotoBean {
+	private String photo_data;
+	private int photo_id;
+	private int is_published;
+	private String photo_title;
+	private String area_id;
+	private String area_name;
+	private int album_id;
+		
+	public PhotoBean(){	
+}
+
+	public String getPhoto_data() {
+		return photo_data;
+	}
+
+	public void setPhoto_data(String photo_data) {
+		this.photo_data = photo_data;
+	}
+
+	public int getPhoto_id() {
+		return photo_id;
+	}
+
+	public void setPhoto_id(int photo_id) {
+		this.photo_id = photo_id;
+	}
+
+	public int getIs_published() {
+		return is_published;
+	}
+
+	public void setIs_published(int is_published) {
+		this.is_published = is_published;
+	}
+
+	public String getPhoto_title() {
+		return photo_title;
+	}
+
+	public void setPhoto_title(String photo_title) {
+		this.photo_title = photo_title;
+	}
+
+	public String getArea_id() {
+		return area_id;
+	}
+
+	public void setArea_id(String area_id) {
+		this.area_id = area_id;
+	}
+
+	public String getArea_name() {
+		return area_name;
+	}
+
+	public void setArea_name(String area_name) {
+		this.area_name = area_name;
+	}
+
+	public int getAlbum_id() {
+		return album_id;
+	}
+
+	public void setAlbum_id(int album_id) {
+		this.album_id = album_id;
+	}
 
 }
