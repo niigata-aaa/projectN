6af922299651573f46bd9fa3388bdf44ae59a585
emtech-user <emtech-user@PC-number>
2026-05-28 15:46:21 +0900

    connectionmanager

--- projectN/src/main/java/model/dao/ConnectionManager.java
+++ projectN/src/main/java/model/dao/ConnectionManager.java
@@ -1,5 +1,31 @@
 package model.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionManager {
 
+	// データベースURL
+	private static final String URL = "jdbc:mysql://localhost:3306/projectN_db";
+
+	 // ユーザ
+	private static final String USER = "HIRO";
+
+	// パスワード
+	private static final String PASSWORD = "BAYMAX";
+
+	/**
+	 * データベースへの接続を取得して返します。
+	 * @return コネクション
+	 * @throws SQLException
+	 * @throws ClassNotFoundException
+	 */
+	public static Connection getConnection() throws SQLException, ClassNotFoundException {
+
+		// JDBCドライバの読み込み
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		return DriverManager.getConnection(URL, USER, PASSWORD);
+
+	}
 }
