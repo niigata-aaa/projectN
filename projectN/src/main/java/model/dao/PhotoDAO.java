fb90e7787c2fa9776c749958ad3b02a70e091649
emtech-user <emtech-user@PC-number>
2026-05-28 17:00:30 +0900

    deletePhoto

--- projectN/src/main/java/model/dao/PhotoDAO.java
+++ projectN/src/main/java/model/dao/PhotoDAO.java
@@ -6,25 +6,42 @@ import java.sql.SQLException;
 
 public class PhotoDAO {
 	//写真を追加するメソッド
-	public int insertPhoto(int album_id, int area_id, String photo_data)throws ClassNotFoundException, SQLException {
-		
+	public int insertPhoto(int album_id, int area_id, String photo_data) throws ClassNotFoundException, SQLException {
+
 		String sql = "INSERT INTO t_photo VALUES (null, ?, ?. ?, 0, null)";
-		
+
+		int count = 0;
+
+		try (Connection con = ConnectionManager.getConnection();
+				PreparedStatement pstmt = con.prepareStatement("INSERT INTO m_employee VALUES(?, ?, ?, ?)");) {
+
+			// プレースホルダへの値の設定
+			pstmt.setInt(1, album_id);
+			pstmt.setInt(2, area_id);
+			pstmt.setString(3, photo_data);
+
+			// SQLステートメントの実行
+			count = pstmt.executeUpdate();
+		}
+
+		return count;
+	}
+	
+	// 写真を削除する
+	public int deletePhoto(int photo_id) throws ClassNotFoundException, SQLException {
 		int count = 0;
 		
-		try(Connection con = ConnectionManager.getConnection();
-				PreparedStatement pstmt = con.prepareStatement("INSERT INTO m_employee VALUES(?, ?, ?, ?)");){
-			
-
-						// プレースホルダへの値の設定
-						pstmt.setInt(1, album_id);
-						pstmt.setInt(2, area_id);
-						pstmt.setString(3, photo_data);
-	
-						// SQLステートメントの実行
-						count = pstmt.executeUpdate();
-		}
+		String sql = "DELETE FROM t_photo WHERE photo_id = ?";
 		
+		try (Connection con = ConnectionManager.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql);) {
+
+			// プレースホルダへの値の設定
+			pstmt.setInt(1, photo_id);
+
+			// SQLステートメントの実行
+			count = pstmt.executeUpdate();
+		}
 		return count;
 	}
 }
