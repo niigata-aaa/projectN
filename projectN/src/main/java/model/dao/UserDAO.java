e126b0403a49da42516a6750582d2f9a35d3c307
emtech-user <emtech-user@PC-number>
2026-05-28 15:03:15 +0900

    初回コミット廣井

--- projectN/src/main/java/model/dao/UserDAO.java
+++ projectN/src/main/java/model/dao/UserDAO.java
@@ -1,5 +1,22 @@
 package model.dao;
 
-public class UserDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+public class UserDAO {//指定されたuser_idのユーザを削除するDAO
+	public int delete(String user_id) throws SQLException{
+		int count = 0;
+		
+		String sql = "DELETE FROM m_user WHERE user_id = ?";
+		
+		try(Connection con = ConnectionManager.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql)){
+			
+			pstmt.setString(1, user_id);
+			
+			count = pstmt.executeUpdate();
+		}
+		return count;
+	}
 }
