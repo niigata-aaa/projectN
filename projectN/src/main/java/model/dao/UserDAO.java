49b6e7e99e15b0c5a695cbe59bf16a0f2fdcc5f7
emtech-user <emtech-user@PC-number>
2026-05-29 09:13:10 +0900

    commit

--- projectN/src/main/java/model/dao/UserDAO.java
+++ projectN/src/main/java/model/dao/UserDAO.java
@@ -46,7 +46,31 @@ public class UserDAO {//指定されたuser_idのユーザを削除するDAO
 				
 				
 	}
-
+	
+//	public class Proen {//新しいユーザの登録
+//		
+//		public int insert(String user_id,String password)throws SQLException,ClassNotFoundException{
+//			int count = 0;
+//			
+//			String sql = "INSERT INTO m_user VALUE(?,?)";//役職名はここではつかう？
+//			
+//			try(Connection con = ConnectionManager.getConnection();
+//					PreparedStatement pstmt = con.prepareStatement(sql)){
+//				
+//				String user_id = user.getUser_id();
+//				String password = user.getPassword();
+//				
+//				pstmt.setString(1,user_id);
+//				pstmt.setString(2,password);
+//				
+//				count = pstmt.executeUpdate();
+//				
+//				}
+//			
+//				return count;
+//			}
+//
+//	}
 }
 
 
