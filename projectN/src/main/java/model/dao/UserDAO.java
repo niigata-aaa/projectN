56547bd26d3ff501de17106a4b425a1e89231662
emtech-user <emtech-user@PC-number>
2026-05-29 12:05:16 +0900

    commit

--- projectN/src/main/java/model/dao/UserDAO.java
+++ projectN/src/main/java/model/dao/UserDAO.java
@@ -69,30 +69,86 @@ public class UserDAO {//指定されたuser_idのユーザを削除するDAO
 		return false;
 	}
 	
-//	public class Proen {//新しいユーザの登録
-//		
-//		public int insert(String user_id,String password)throws SQLException,ClassNotFoundException{
-//			int count = 0;
-//			
-//			String sql = "INSERT INTO m_user VALUE(?,?)";//役職名はここではつかう？
-//			
-//			try(Connection con = ConnectionManager.getConnection();
-//					PreparedStatement pstmt = con.prepareStatement(sql)){
-//				
-//				String user_id = user.getUser_id();
-//				String password = user.getPassword();
-//				
-//				pstmt.setString(1,user_id);
-//				pstmt.setString(2,password);
-//				
-//				count = pstmt.executeUpdate();
-//				
-//				}
-//			
-//				return count;
-//			}
-//
-//	}
+
+		//ユーザの登録
+		public int insertUser(String user_id,String password)throws SQLException,ClassNotFoundException{
+			int count = 0;
+			
+			String sql = "INSERT INTO m_user VALUE(?,?)";
+			
+			try(Connection con = ConnectionManager.getConnection();
+					PreparedStatement pstmt = con.prepareStatement(sql)){
+				
+				pstmt.setString(1,user_id);
+				pstmt.setString(2,password);
+				
+				count = pstmt.executeUpdate();
+				
+			}
+			
+				return count;
+		}
+	
+		
+		//ユーザの登録（管理者）
+		public int insertAdmin(String user_id,String password,int post_id)throws SQLException,ClassNotFoundException{
+				int count = 0;
+					
+				String sql = "INSERT INTO m_user VALUE(?,?,?)";
+					
+				try(Connection con = ConnectionManager.getConnection();
+					PreparedStatement pstmt = con.prepareStatement(sql)){
+						
+						
+					pstmt.setString(1,user_id);
+					pstmt.setString(2,password);
+					pstmt.setInt(3,post_id);
+						
+					count = pstmt.executeUpdate();
+						
+					}
+					
+					return count;
+			}
+		//displaySelectUser(String user_id)のDAO
+		public UserBean displaySelectUser(String user_id) throws SQLException, ClassNotFoundException {
+			UserBean user = new UserBean();
+			String sql = "SELECT * FROM m_user WHERE user_id = ?";
+			//データベースへの接続の取得、PreparedStatementの取得
+			try (Connection con = ConnectionManager.getConnection();
+					PreparedStatement pstmt = con.prepareStatement(sql)) {
+				//プレースホルダへの値の設定
+				pstmt.setString(1, user_id);
+				//SQLステートメントの実行
+				ResultSet res = pstmt.executeQuery();
+				//結果の操作
+				while (res.next()) {
+					user.setUser_id(res.getString("user_id"));
+					user.setPost_id(res.getInt("post_id"));
+				}
+			}
+			return user;
+		}
+		
+		
+		//displaySelectUser(int post_id)のDAO
+		public List<UserBean> displaySelectUser(int post_id) throws SQLException, ClassNotFoundException {
+			List<UserBean> displaySelectUser = new ArrayList<UserBean>();
+			//データベースへの接続の取得、Statementの取得、SQLステートメントの実行
+			try (Connection con = ConnectionManager.getConnection();
+					Statement stmt = con.createStatement();
+					ResultSet res = stmt.executeQuery("SELECT * FROM m_post")) {
+				while (res.next()) {
+					UserBean user = new UserBean();
+					user.setUser_id(res.getString("user_id"));
+					user.setPost_id(res.getInt("post_id"));
+					displaySelectUser.add(user);
 }
+			}
+			return displaySelectUser;
+		}
+
+	}
+
 
 
