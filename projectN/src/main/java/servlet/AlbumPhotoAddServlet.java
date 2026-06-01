af26a1d9bf9d52406786219a1bc6702d1e3b4d5c
emtech-user <emtech-user@PC-number>
2026-06-01 16:18:33 +0900

    写真追加複数

--- projectN/src/main/java/servlet/AlbumPhotoAddServlet.java
+++ projectN/src/main/java/servlet/AlbumPhotoAddServlet.java
@@ -4,6 +4,9 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -22,27 +25,24 @@ import model.entity.AlbumBean;
  * Servlet implementation class AlbumPhotoAddServlet
  */
 @WebServlet("/album-photo-add")
-@MultipartConfig(
-		maxFileSize=10000000,
-		maxRequestSize=10000000,
-		fileSizeThreshold=10000000
-	)
+@MultipartConfig(maxFileSize = 10000000, maxRequestSize = 10000000, fileSizeThreshold = 10000000)
 
 public class AlbumPhotoAddServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AlbumPhotoAddServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public AlbumPhotoAddServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
@@ -50,43 +50,48 @@ public class AlbumPhotoAddServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		int cnt = 0;
-		
-		
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		//セッション取得
 		HttpSession session = request.getSession();
-		
-		AlbumBean albumbean = (AlbumBean)session.getAttribute("selectAlbum");
-		
+
+		AlbumBean albumbean = (AlbumBean) session.getAttribute("selectAlbum");
+
 		//album_id取得
 		int album_id = albumbean.getAlbum_id();
 		//area_id取得
 		int area_id = albumbean.getArea_id();
-		
-		Part part = request.getPart("photo");
-		String photo = Paths.get(part.getSubmittedFileName()).getFileName().toString();
-		String photo_data = photo.isEmpty() ? "" : photo;
-		
-		String path = getServletContext().getRealPath("/photo");
-		part.write(path + File.separator + photo_data);
 
-		
-		
-		PhotoDAO photodao = new PhotoDAO(photo_data);
-		
+		// 画像をパスで取得
+		List<String> photoDataList = new ArrayList<String>();
+		Collection<Part> partList = request.getParts();
+		for (Part part : partList) {
+			if ("photo".equals(part.getName()) && part.getSubmittedFileName() != null) {
+				String photo = Paths.get(part.getSubmittedFileName()).getFileName().toString();
+
+				if (!photo.isEmpty()) {
+					String path = getServletContext().getRealPath("/photo");
+					part.write(path + File.separator + photo);
+
+					photoDataList.add(photo);
+				}
+			}
+		}
+
+		PhotoDAO photodao = new PhotoDAO();
+
 		try {
-			//insertメソッド
-			cnt = photodao.insertPhoto(album_id, area_id, photo_data);
-		} catch (ClassNotFoundException | SQLException e){
+			for (String photo_data : photoDataList) {
+				photodao.insertPhoto(album_id, area_id, photo_data);
+			}
+		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
-		
-		request.setAttribute("cnt", cnt);
-		
+
 		RequestDispatcher rd = request.getRequestDispatcher("album-photo-add-comp.jsp");
 		rd.forward(request, response);
-		
+
 	}
 
 }
